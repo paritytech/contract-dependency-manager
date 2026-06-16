bd0db1f029adfbb77cb57023edb5596cb53cd768
Charles Hetterich <hetterich.charles@gmail.com>
2026-06-16 13:05:23 +0200

    updated registry addresses

--- src/lib/env/src/registry.ts
+++ src/lib/env/src/registry.ts
@@ -2,7 +2,7 @@ export type ProductSdkEnvironment = "paseo" | "summit";
 
 const POLKADOT_REGISTRY_ADDRESS = "";
 const PASEO_REGISTRY_ADDRESS = "0xf62c2ece29cd8df2e10040ecfa5a894a5c5d9cb0";
-const W3S_REGISTRY_ADDRESS = "";
+const W3S_REGISTRY_ADDRESS = "0xa5747e60ae27f93e92019e4021abfc4957050141";
 const LOCAL_REGISTRY_ADDRESS = "";
 
 export function getRegistryAddress(name = "paseo"): string {
